d7f918353505cd9dbc6d6cbc24abff26dd0a9f76
Shelley Brook <sbrook13@gmail.com>
2020-06-27 22:06:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,5 +6,5 @@ function updatePlaylist(list,a,b){
   return Object.assign({},playlist,{artistName:[a,...artistName]},{songTitle:[b,...songTitle]})
 }
 playlist.artistName = function(a){
-  artistName:[a,...artistName]
+  artistName:[a,...artistName];
 }

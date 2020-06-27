8af896a3611bcdce8f6c1c13bc247fc014fa392c
Shelley Brook <sbrook13@gmail.com>
2020-06-27 21:57:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,6 @@ var playlist = {
   artistName: "Van Morrison",
   songTitle: "Moondance"
 };
+function updatePlaylist(targetObject,updatesObject){
+  return Object.assing({},playlist,{artistName:[updatesObject)
+}

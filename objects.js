22d8acbc58047d5661e29b91e3eed04ddbe6bdef
Shelley Brook <sbrook13@gmail.com>
2020-06-27 22:02:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   artistName: "Van Morrison",
   songTitle: "Moondance"
 };
-function updatePlaylist(playlist,artistName,songTitle){
-  return Object.assign({},playlist,{artistName:['Justin Timberlake',...artistName]},{songTitle:['Cant Stop The Feeling',...songTitle]})
+function updatePlaylist(list,a,b){
+  return Object.assign({},list,{artistName:[a,...artistName]},{songTitle:[b,...songTitle]})
 }
